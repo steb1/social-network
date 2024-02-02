b9a3d48f7da2c31798d5ad14f031b2cef6aa4694
Ivan <xolsp123@gmail.com>
2024-02-02 17:16:14 +0000

    Authorization

--- frontend/middleware/authMiddleware.js
+++ frontend/middleware/authMiddleware.js
@@ -4,30 +4,34 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 const authMiddleware = (handler, serverUrl) => async (req, res) => {
-	const cookieStore = cookies();
-	const token = cookieStore.get("cocial-network");
+    const cookieStore = cookies();
+    const token = cookieStore.get("social-network");
 
-	console.log(token);
+    console.log(token);
 
-	if (!token) {
-		redirect("/auth/signin");
-	}
+    if (!token) {
+        console.log("No token");
+        redirect("/auth/signin");
+    }
 
-	const response = await fetch(serverUrl, {
-		method: "POST",
-		headers: {
-			Authorization: `Bearer ${token}`,
-			"Content-Type": "application/json",
-		},
-	});
+    const response = await fetch(serverUrl, {
+        method: "GET",
+        headers: {
+            Authorization: `${token}`,
+        },
+    });
 
-	if (response.status === 200) {
-		// Si le serveur renvoie OK, laisser passer la requête
-		return handler(req, res);
-	} else {
-		// Si le serveur renvoie 401, rediriger vers la page de login
-		redirect("/auth/signin");
-	}
+    if (response.status === 200) {
+        // Si le serveur renvoie OK, laisser passer la requête
+        console.log("OKKK");
+        return handler(req, res);
+    } else if (response.status === 400) {
+        // Si le serveur renvoie 401, rediriger vers la page de login
+        console.log("401");
+        redirect("/auth/signin");
+    } else {
+        console.log(response);
+    }
 };
 
 export default authMiddleware;
