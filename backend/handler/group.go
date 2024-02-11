357b74f6ac1aeebc4f83bae1b2facf76709578eb
lomalack <you@example.com>
2024-02-11 15:22:45 +0000

    add : group resquest

--- backend/handler/group.go
+++ backend/handler/group.go
@@ -1,11 +1,13 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"server/lib"
 	"server/models"
 	"strconv"
+	"time"
 )
 
 func HandleOptions(w http.ResponseWriter, r *http.Request) {
@@ -43,9 +45,75 @@ func HandleGetAllGroups(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	groups := models.GroupRepo.GetAllGroup()
-	fmt.Println("is here")
 
 	if ok {
 		lib.WriteJSONResponse(w, groups)
+	}
+}
+
+func HandleCreateMembership(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodOptions {
+		HandleOptions(w, r)
+		return
+	}
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	_, ok := IsAuthenticated(r)
+
+	var apiError ApiError
+	sessionToken := r.Header.Get("Authorization")
+	session, err := models.SessionRepo.GetSession(sessionToken)
+	if err != nil {
+		apiError.Error = "Go connect first !"
+		WriteJSON(w, http.StatusUnauthorized, apiError)
+		return
+	}
+	userId, err := strconv.Atoi(session.UserID)
+	if err != nil {
+		apiError.Error = "Error getting user."
+		WriteJSON(w, http.StatusUnauthorized, apiError)
+		return
+	}
+
+	var requestBody map[string]interface{}
+	err = json.NewDecoder(r.Body).Decode(&requestBody)
+	if err != nil {
+		http.Error(w, "Erreur lors de la lecture du corps de la requête", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Println(requestBody["groupid"], "-------- request body")
+
+	v, ok_ := requestBody["groupid"]
+	groupid, _ :=  strconv.Atoi(fmt.Sprintf("%v", v))
+	if !ok_ {
+		http.Error(w, "Paramètre groupid manquant ou invalide", http.StatusBadRequest)
+		return
+	}
+	
+	// Récupérer groupid
+	exist := models.MembershipRepo.CheckIfMembershispExist( userId, groupid)
+	fmt.Println("is here")
+	if ok && !exist {
+		var Membership models.Membership
+		
+		Membership.GroupID = groupid
+		
+		if err != nil {
+			return
+		}
+
+		Membership.UserID = userId
+		Membership.JoinedAt = time.Now().String()
+		Membership.InvitationStatus = "request"
+		Membership.MembershipStatus = "pending"
+
+		err = models.MembershipRepo.CreateMembership(&Membership)
+
+		if err != nil {
+			fmt.Println("Membership non crée !")
+		}
 	}
 }
--- frontend/app/components/groupOption.jsx
+++ frontend/app/components/groupOption.jsx
@@ -1,18 +1,49 @@
+import config from "@/config";
+
 export const GroupOption = ( {group} ) => {
 
     return (
-        <div id={group.group_id} className="card card-compact w-36 w-96 bg-base-100 shadow-xl">
+        <div id={group.group_id} className="card card-compact w-36 bg-base-100 shadow-xl">
             <figure><img src="https://i0.wp.com/www.iedunote.com/img/28051/reference-groups.jpg?fit=1080%2C720&quality=100&ssl=1" alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{group.title}</h2>
                 <p>{group.description}</p>
                 <div className="card-actions justify-end">
-                <button className="btn btn-primary">Join</button>
+                <button onClick={() => handleGroupRequest(group.group_id)} className="btn btn-primary">Join</button>
                 </div>
             </div>
         </div>
-        
     )
 }
+
+async function  handleGroupRequest  (groupid) {
+    console.log("clicked");
+    let  token = document.cookie.split("=")[1]
+      
+    if (token) {
+      // Use the token as needed
+      console.log('Token:', token);
+    } else {
+      console.log('Token not found in cookies');
+    }
+
+    try {
+      const response = await fetch(config.serverApiUrl + "createMembership", {
+        method: "POST",
+        headers: {
+          'Authorization': token,
+        },
+        credentials: "include",
+        body: JSON.stringify({ groupid: groupid })
+      });
+
+      if (response.ok) {
+        console.log("ooooooooook");
+      }
+    } catch (error) {
+        console.error("Error while fetching groups:", error);
+      }
+}
+
 
 
