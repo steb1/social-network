c54f8c8c3e7d900dd0127de2f1844389417b8d7f
magniang <naingpmaxgatte@gmail.com>
2024-02-15 02:19:08 +0000

    Design of message finished

---
+++ frontend/app/messages/DateMessage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DateMessage = () => {
+	return (
+		<div className="flex justify-center ">
+			<div className="font-medium text-gray-500 text-sm dark:text-white/70">April 8,2023,6:30 AM</div>
+		</div>
+	);
+};
+
+export default DateMessage;
