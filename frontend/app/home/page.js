c6a6d8484f61f74cd80ddf841fbe5633011a8197
Ivan <xolsp123@gmail.com>
2024-02-19 18:30:41 +0000

    Refactor front in Layout in order to have only one Sidebar & Header

--- frontend/app/home/page.js
+++ frontend/app/home/page.js
@@ -1,5 +1,3 @@
-import Header from "../components/header";
-import Sidebar from "../components/sidebar";
 import AddStory from "../components/addStory";
 import { PostText, PostPlaceholder } from "../components/posts";
 import { Rightbar } from "../components/rightbar";
@@ -7,27 +5,22 @@ import "../../public/js/script.js";
 import "../../public/js/simplebar.js";
 
 const HomePage = () => {
-	return (
-		<div id="wrapper">
-			{/* Header */}
-			<Header />
-			<Sidebar />
-			<div className="flex mt-5">
-				<div className="flex-1 pt-8 px-5 md:max-w-[580px] xl:space-y-6 space-y-3 ml-80 ">
-					{/* Add Story Section */}
-					<AddStory />
-					{/* Posts Section */}
-					<PostText />
-					<PostPlaceholder />
-					{/* Modal */}
-				</div>
-				{/* Fixed Rightbar */}
-				<div className="block h-full mt-20 ml-20 max-sm:hidden max-md:hidden max-lg:hidden ">
-					<Rightbar />
-				</div>
-			</div>
-		</div>
-	);
+    return (
+        <div className='flex mt-5'>
+            <div className='flex-1 pt-8 px-5 md:max-w-[580px] xl:space-y-6 space-y-3 ml-80 '>
+                {/* Add Story Section */}
+                <AddStory />
+                {/* Posts Section */}
+                <PostText />
+                <PostPlaceholder />
+                {/* Modal */}
+            </div>
+            {/* Fixed Rightbar */}
+            <div className='block h-full mt-20 ml-20 max-sm:hidden max-md:hidden max-lg:hidden '>
+                <Rightbar />
+            </div>
+        </div>
+    );
 };
 
 export default HomePage;
--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -7,25 +7,33 @@ import "../public/js/script.js";
 import "../public/js/simplebar.js";
 import "../public/js/uikit.min.js";
 
+import Sidebar from "./components/sidebar";
+import Header from "./components/header";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-	title: "Social network",
-	description: "The social network for 01",
+    title: "Social network",
+    description: "The social network for 01",
 };
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang="en">
-			<head>
-				<script src="/js/uikit.min.js"></script>
-				<script src="/js/simplebar.js"></script>
-				<script src="/js/script.js"></script>
-				{/* <!-- Ion icon --> */}
-				<script noModule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-				<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-			</head>
-			<body className={inter.className}>{children}</body>
-		</html>
-	);
+    return (
+        <html lang='en'>
+            <head></head>
+            <body className={inter.className}>
+                <div id='wrapper'>
+                    <Header />
+                    <Sidebar />
+                    <main>{children}</main>
+                </div>
+                <script src='/js/uikit.min.js'></script>
+                <script src='/js/simplebar.js'></script>
+                <script src='/js/script.js'></script>
+                {/* <!-- Ion icon --> */}
+                <script noModule src='https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js'></script>
+                <script type='module' src='https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js'></script>
+            </body>
+        </html>
+    );
 }
