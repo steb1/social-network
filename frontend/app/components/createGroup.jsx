59c795f2506ba2916c876c4e79255d0421337068
Louis Malack <sebago1214@gmail.com>
2024-03-05 04:34:53 +0000

    correct dark mode

--- frontend/app/components/createGroup.jsx
+++ frontend/app/components/createGroup.jsx
@@ -29,7 +29,7 @@ export const CreateGroup = () => {
 	}, []);
 
 	return (
-		<Link href="">
+		<Link href="" className="dark:hover:text-slate-800">
 			<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
 				<path
 					fill="currentColor"
--- frontend/app/groups/components/member.jsx
+++ frontend/app/groups/components/member.jsx
@@ -1,7 +1,7 @@
 export const Members = ({ member }) => {
     return (
         <div className="flex flex-row gap-2">
-            <div className="flex flex-row h-16 px-1 bg-white gap-3 w-[30rem] border-2 items-center rounded-2xl ">
+            <div className="flex flex-row h-16 px-1 dark:bg-slate-800 bg-white gap-3 w-[30rem] border-2 items-center rounded-2xl ">
                 <div className="w-9 h-9 rounded-full mr-7" >
                     <img src={ !member.avatar ?  "../assets/images/avatars/avatar-5.jpg" : member.avatar } className="rounded-full ml-2" alt="" />
                 </div>
