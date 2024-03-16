b93d6e1d99c994ab57ea8fc41a2995414052cae0
yayediop <mamyta2000@outlook.fr>
2024-03-16 13:51:38 +0000

    fix: logout icon color when the theme is dark

--- frontend/app/components/Logout.jsx
+++ frontend/app/components/Logout.jsx
@@ -5,43 +5,31 @@ import config from "@/config";
 import { useWebSocketContext } from "@/public/js/websocketContext";
 
 const Logout = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    const handleLogout = () => {
-        document.cookie = `${config.cookieName}=; path=/; max-age=0; Secure`;
-        localStorage.clear();
-        router.replace("/auth/signin");
-        const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocketContext();
-        closeWebSocket();
-        sendJsonMessage({ command: "logout" });
-    };
+  const handleLogout = () => {
+    document.cookie = `${config.cookieName}=; path=/; max-age=0; Secure`;
+    localStorage.clear();
+    router.replace("/auth/signin");
+    const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocketContext();
+    closeWebSocket();
+    sendJsonMessage({ command: "logout" });
+  };
 
   return (
-    // <div className="relative cursor-pointer shrink-0" onClick={handleLogout}>
-    // 	<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 16 16">
-    // 		<path fill="currentColor" fillRule="evenodd" d="M11.02 3.77v1.56l1-.99V2.5l-.5-.5h-9l-.5.5v.486L2 3v10.29l.36.46l5 1.72L8 15v-1h3.52l.5-.5v-1.81l-1-1V13H8V4.71l-.33-.46L4.036 3h6.984zM7 14.28l-4-1.34V3.72l4 1.34zm6.52-5.8H8.55v-1h4.93l-1.6-1.6l.71-.7l2.47 2.46v.71l-2.49 2.48l-.7-.7z" clipRule="evenodd" />
-    // 	</svg>
-    // </div>
     <div
-	className=" cursor-pointer "
+      className=" cursor-pointer dark:text-white"
       onClick={handleLogout}
       style={{ width: "100%", height: "100%", fill: "black" }}
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        enableBackground="new 0 0 24 24"
-        height={24}
+        fill="none"
         viewBox="0 0 24 24"
-        width={32}
-        focusable="false"
-        style={{
-          pointerEvents: "none",
-          display: "block",
-          width: "100%",
-          height: "100%",
-        }}
-      >
-        <path d="M20 3v18H8v-1h11V4H8V3h12zm-8.9 12.1.7.7 4.4-4.4L11.8 7l-.7.7 3.1 3.1H3v1h11.3l-3.2 3.3z" />
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-7 h-7">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9" />
       </svg>
     </div>
   );
